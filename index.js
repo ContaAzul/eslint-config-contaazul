9df3579647df7f0ad0f9f6e24e638c7aa86f03ec
Jhon Mike <developer@jhonmike.com.br>
2018-01-25 22:36:17 -0200

    rules conta azul

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 module.exports = {
+  rules: {
+    "default-case" : 1,
+    "max-len" : 1,
+    "no-new" : 1,
+    "consistent-return" : 1,
+    "no-confusing-arrow" : 1,
+    "indent" : ["error", 2],
+    "no-tabs" : 0,
+    "no-underscore-dangle" : 0,
+    "no-plusplus" : 0,
+    "import/prefer-default-export" : 0,
+    "radix" : [ 2, "as-needed" ],
+    "class-methods-use-this" : 0,
+    "no-param-reassign" : 1,
+    "no-console": ["warn", { "allow": ["error"] }],
+    "no-use-before-define": ["error", { "functions": false, "classes": true }],
+    "prefer-destructuring": ["warn", {"object": true, "array": true}],
+  },
   "extends": ["airbnb-base"]
 };
